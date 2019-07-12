b7496ee6c468d77a9bb348e9bd5c093327c2c74b
Anil Dhurjaty <adhurjaty@gmail.com>
2019-07-11 20:27:38 -0700

    targets es6 for jest

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,12 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 
-ReactDOM.render(React.createElement("h2", null, "Hello world!"), document.body);
+function* range(n: number) {
+    for (let i = 0; i < n; i++) {
+        yield i;        
+    }
+}
+
+let arr: number[] = Array.from(range(5));
+ReactDOM.render(React.createElement("h2", null, arr.map((a) => '' + a).join(' ')), 
+    document.body);
