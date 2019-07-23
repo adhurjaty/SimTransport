f8a552272bc186d2b5a035cc45594cfb8fd228b3
Anil Dhurjaty <adhurjaty@gmail.com>
2019-07-22 21:04:04 -0700

    gets turning test to pass

--- src/simulator/car_controller.ts
+++ src/simulator/car_controller.ts
@@ -7,6 +7,7 @@ import { getCoord, getAddress } from "./simulator_helpers";
 import { Speed } from "../primitives";
 import { TICK_DURATION, INTERSECTION_SIZE } from "../constants";
 import { RoadDirection } from "../enums";
+import Coord from "../models/coord";
 
 export default class CarController {
     public path: PathInstruction[];
@@ -28,24 +29,29 @@ export default class CarController {
     }
 
     makeDecision(): void {
+        if(this.path.length == 0) {
+            return;
+        }
+
         let destAddr: Address = getAddress(this.world.network, this.path[0].location);
 
         let distToWaypoint: number = Math.abs(this.car.address.distance 
             - destAddr.distance);
 
+        distToWaypoint -= INTERSECTION_SIZE;
         if(this.path.length == 1 && distToWaypoint <= Number.EPSILON) {
             this.path = [];
             this.car.setSpeed(new Speed(0));
             return;
         }
         
-        distToWaypoint -= INTERSECTION_SIZE;
         if(distToWaypoint <= Number.EPSILON) {
-            this.path.splice(0, 1);
+            let lastInstruction: PathInstruction = this.path.splice(0, 1)[0];
 
-            this.car.address = this.getNextAddress();
+            this.car.address = this.getNextAddress(lastInstruction.location);
             this.car.direction = this.path[0].direction;
             this.car.setSpeed(this.getSpeedLimit());
+            return;
         }
 
         // if car is so close to checkpoint that it will overshoot next time step
@@ -58,9 +64,9 @@ export default class CarController {
         return Math.abs(this.car.velocity.mps()) / TICK_DURATION;
     }
 
-    private getNextAddress(): Address {
+    private getNextAddress(intersectionLoc: Coord): Address {
         let addr: Address = getAddress(this.world.network, 
-            this.path[0].location);
+            intersectionLoc);
 
         addr.distance += (this.path[0].direction == RoadDirection.Charm
             ? 1 : -1) * INTERSECTION_SIZE;
