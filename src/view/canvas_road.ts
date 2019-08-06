3fce61ef562ac93d6bfe1951ac980d3b0d5ca644
Anil Dhurjaty <adhurjaty@gmail.com>
2019-08-05 21:46:20 -0700

    scaffolds drawing functions for canvas

---
+++ src/view/canvas_road.ts
@@ -0,0 +1,9 @@
+import Road from "../models/road";
+
+export default class CanvasRoad {
+    constructor(private ctx: CanvasRenderingContext2D, private road: Road) {
+
+    }
+
+    
+}
--- src/view/sim_canvas.tsx
+++ src/view/sim_canvas.tsx
@@ -1,20 +1,44 @@
 import React, { RefObject, createRef } from "react";
+import World from "../simulator/world";
 
-type CanvasProps = {
+interface CanvasProps {
     width: number;
     height: number;
+    world: World;
+}
+
+interface CanvasState {
+    ctx: CanvasRenderingContext2D | null;
 }
 
 export default class SimCanvas extends React.Component<CanvasProps, {}> {
-    private canvasRef: RefObject<HTMLCanvasElement> = createRef<HTMLCanvasElement>(); 
+    private canvasRef: RefObject<HTMLCanvasElement> = createRef<HTMLCanvasElement>();
+    private ctx: CanvasRenderingContext2D;
 
     componentDidMount() {
         const canvas = this.canvasRef.current;
         if(canvas) {
-            const ctx = canvas.getContext("2d");
-            ctx.fillStyle = "black";
-            ctx.fillRect(0, 0, this.props.width, this.props.height);
+            this.ctx = canvas.getContext("2d");
+            this.drawBackground();
         }
+    }
+
+    drawBackground(): void {
+        this.ctx.fillStyle = "green";
+        this.ctx.fillRect(0, 0, this.props.width, this.props.height);
+    }
+    
+    draw(): void {
+        this.drawRoads();
+        this.drawCars();
+    }
+
+    drawRoads(): void {
+        
+    }
+
+    drawCars(): void {
+
     }
 
     render() {
